4f3a770df166ebd2833b0192af6e25065807a710
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-03-11 15:49:29 -0300

    env port added

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lwsdsfk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((error) => console.log(error));
